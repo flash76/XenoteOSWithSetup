e9855b07a2e7caa32c51e1e951ad5436f71ef94e
flash76 <Varunsom@outlook.com>
2017-11-25 16:54:30 -0500

    add welcomes

--- XenoteOS with Setup/Kernel.cs
+++ XenoteOS with Setup/Kernel.cs
@@ -19,7 +19,7 @@ namespace XenoteOS_with_Setup
 
             for (int i = 0; i < 40; i++)
             {
-                dash += "'";
+                dash += "=";
             }
 
             Console.WriteLine("\tWelcome to XenoteOS");
