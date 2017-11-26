01b5c812930436b2ba40352a6d7d6c1dee74fcfc
flash76 <Varunsom@outlook.com>
2017-11-25 20:46:48 -0500

    welcome messages

--- XenoteOS with Setup/Kernel.cs
+++ XenoteOS with Setup/Kernel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Cosmos.Core;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using Sys = Cosmos.System;
 
@@ -11,20 +13,23 @@ namespace XenoteOS_with_Setup
         {
             Console.WriteLine("__   __                 _        _____ _____ \r\n\\ \\ / /                | |      |  _  /  ___|\r\n \\ V /  ___ _ __   ___ | |_ ___ | | | \\ `--. \r\n /   \\ / _ \\ '_ \\ / _ \\| __/ _ \\| | | |`--. \\\r\n/ /^\\ \\  __/ | | | (_) | ||  __/\\ \\_/ /\\__/ /\r\n\\/   \\/\\___|_| |_|\\___/ \\__\\___| \\___/\\____/ \r\n                                             \r\n     ");
             Console.WriteLine("(c) 2017 by Varun Somashekar\n This software is made available under the Apache Licence 2.0");
+            
+            Console.WriteLine("\n\n\t\tWelcome to XenoteOS");
+
+            Console.WriteLine("\n\n\tLet's set you up.");
+            Console.WriteLine("Hold up, we are doing something real quick...");
+
+            //Start the filesystem
+            var fs = new Sys.FileSystem.CosmosVFS();
+            Sys.FileSystem.VFS.VFSManager.RegisterVFS(fs);
+
+            Console.WriteLine("Thanks for that, the filesystem is now enabled. FAT.");
+
         }
-        
+
         protected override void Run()
         {
-            string setupBorder = "";
-
-            for (int i = 0; i < 40; i++)
-            {
-                setupBorder += "=";
-            }
-
-            Console.WriteLine("\tWelcome to XenoteOS");
-
-            Console.WriteLine("Let's set you up.");
+            
         }
     }
 }
